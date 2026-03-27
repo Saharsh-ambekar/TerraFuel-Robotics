4bf69fe58a3685fae0a5e918cc8490e93bc52b99
hp.ADMIN <kaushik.annadata@gmail.com>
2026-03-27 13:11:53 +0530

    First working motor code

--- arduino1_drill_only/arduino1_drill_only.ino
+++ arduino1_drill_only/arduino1_drill_only.ino
@@ -17,17 +17,26 @@
 #include <SoftwareSerial.h>
 SoftwareSerial toDisplay(6, 7); // TX=6, RX=7
 
-#define IN1 2
-#define IN2 3
+#define IN1 9
+#define IN2 8
 #define ENA 10
+#define IN3 2
+#define IN4 4
+#define ENB 3
 
-void doDrill(bool on) {
-  if (on) {
+
+void doDrill(int status) {
+  if (1) {
     digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
     analogWrite(ENA, 180);
     Serial.println("  >> Drill ON");
     toDisplay.println("Drill: ON,Running.....");
-  } else {
+  } else if(2){
+    digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
+    analogWrite(ENA, 180);
+    Serial.println("  >> Drill ON BACK");
+    toDisplay.println("Drill: ON BACK,Running.....");
+  }else{
     digitalWrite(IN1, LOW);  digitalWrite(IN2, LOW);
     analogWrite(ENA, 0);
     Serial.println("  >> Drill OFF");
@@ -35,15 +44,34 @@ void doDrill(bool on) {
   }
 }
 
+void doSens(int status){
+  if (1){
+    digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+    analogWrite(ENA, 180);
+    Serial.println("  >> Sens ON");
+    toDisplay.println("SENS: ON, Running.....");
+  }else if(2){
+    digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH);
+    analogWrite(ENA, 180);
+    Serial.println("  >> Sens ON BACK");
+    toDisplay.println("SENS: ON BACK, Running.....");
+  }else{
+    digitalWrite(IN3, LOW); digitalWrite(IN4, LOW);
+    analogWrite(ENA, 0);
+    Serial.println("  >> Sens OFF");
+    toDisplay.println("SENS: OFF, Stopped. ");
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   toDisplay.begin(9600);
-  pinMode(IN1, OUTPUT); pinMode(IN2, OUTPUT); pinMode(ENA, OUTPUT);
+  
   digitalWrite(IN1, LOW); digitalWrite(IN2, LOW);
 
   Serial.println("================================");
   Serial.println("  Drill System Ready            ");
-  Serial.println("  drill on / drill off          ");
+  Serial.println("  drill on / drill off          "); 
   Serial.println("================================");
   toDisplay.println("Drill System,Ready...");
 }
@@ -53,8 +81,10 @@ void loop() {
     String cmd = Serial.readStringUntil('\n');
     cmd.trim();
 
-    if      (cmd == "drill on")  doDrill(true);
-    else if (cmd == "drill off") doDrill(false);
+    if      (cmd == "drill on"){  doDrill(1);
+    delay()
+    }
+    else if (cmd == "drill off") doDrill(0);
     else {
       Serial.println("  Unknown. Use: drill on / drill off");
     }
